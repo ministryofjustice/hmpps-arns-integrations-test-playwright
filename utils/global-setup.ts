5ba8698e6721c491ac586a250b04c17a55905d80
JY0280 <jean-yves@hippodigital.co.uk>
2025-10-02 10:33:18 +0100

    Fixed global setup

--- utils/global-setup.ts
+++ utils/global-setup.ts
@@ -8,25 +8,21 @@ let clientId: string;
 let clientSecret: string;
 
 async function globalSetup() {
-  if (process.env.CI) {
-    // running in GitHub Actions
-    clientId = process.env.SP_CLIENT_ID!;
-    clientSecret = process.env.SP_CLIENT_SECRET!;
-  } else {
-    // running locally with kubectl
-    const clientId = execSync(
-      process.env.CLIENT_ID_SCRIPT
-    ).toString().trim();
-
-    const clientSecret = execSync(
-      process.env.CLIENT_SECRET_SCRIPT
-    ).toString().trim();
+  try {
+    if (process.env.CI) {
+      // running in GitHub Actions
+      clientId = process.env.SP_CLIENT_ID!;
+      clientSecret = process.env.SP_CLIENT_SECRET!;
+    } else {
+      // running locally with kubectl
+      clientId = execSync(process.env.CLIENT_ID_SCRIPT!).toString().trim();
+      clientSecret = execSync(process.env.CLIENT_SECRET_SCRIPT!).toString().trim();
+    }
 
     console.log('Fetched client ID and client secret');
 
     // Request OAuth 2.0 token using client credentials
-    const tokenUrl = process.env.TOKEN_URL;
-
+    const tokenUrl = process.env.TOKEN_URL!;
     const params = new URLSearchParams();
     params.append('grant_type', 'client_credentials');
 
@@ -44,11 +40,10 @@ async function globalSetup() {
     }
 
     const tokenData = await resp.json();
-    console.log('Access token received')
+    console.log('Access token received');
 
     // Save token to file
     fs.writeFileSync(TOKEN_PATH, JSON.stringify(tokenData, null, 2), 'utf-8');
-
   } catch (err) {
     console.error('Global setup failed:', err);
     throw err;
