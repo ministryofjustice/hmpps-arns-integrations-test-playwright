ddaea6c7ba2c81be9ca42c40b1a7f9deb71b5dff
JY0280 <jean-yves@hippodigital.co.uk>
2025-10-28 16:10:17 +0000

    Extended test feature to include SP steps updates

--- page-objects/mpop-pages.ts
+++ page-objects/mpop-pages.ts
@@ -13,10 +13,10 @@ export class MpopPages {
         private passwordInput: Locator = page.locator('#password'),
         private signInButton: Locator = page.locator('#submit'),
         private managePeopleOnProbationLink = page.locator('#hmpps-manage-supervisions'),
-        private topNavCasesLink = page.locator('body > div.moj-primary-navigation > div > div.moj-primary-navigation__nav > nav > ul > li:nth-child(2) > a'),
-        private crnLink = page.locator('#main-content > div > div > table > tbody > tr:nth-child(3) > td:nth-child(1) > a'),
-        private riskAndPlanLink = page.locator('#main-content > div.govuk-grid-row > div > nav > ul > li:nth-child(5) > a'),
-        private sentencePlanLink = page.locator('#main-content > div.govuk-grid-row.govuk-\!-margin-top-4 > div.govuk-grid-column-one-third > section > div > p:nth-child(2) > a');
+        private topNavCasesLink = page.getByRole('link', { name: 'Cases', exact: true }),
+        private crnLink = page.getByRole('link', { name: 'Howe, Silvia' }),
+        private riskAndPlanLink = page.getByRole('link', { name: 'Risk and plan' }),
+        private sentencePlanLink = page.getByRole('link', { name: 'View the sentence plan (opens' }),
     ) { }
 
     async navigateToMpopLink() {
@@ -41,16 +41,6 @@ export class MpopPages {
         await this.crnLink.click();
     }
 
-    async checkPageTitle() {
-        // wait for new tab to open
-        const newTabPromise = this.page.waitForEvent('popup');
-        const newTab = await newTabPromise;
-        // wait for Load 
-        await newTab.waitForLoadState();
-        newTabGlobal = newTab;
-        await expect(newTab).toHaveTitle('Overview - Manage people on probation');
-    }
-
     async clickRiskAndPlanLink() {
         await this.riskAndPlanLink.click();
     }
@@ -58,5 +48,4 @@ export class MpopPages {
     async clickSentencePlanLink() {
         await this.sentencePlanLink.click();
     }
-
 }
