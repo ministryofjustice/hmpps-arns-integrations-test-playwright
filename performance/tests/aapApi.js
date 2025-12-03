a0af25a1389023f671b0d45de37ab95be56ad702
JY0280 <jean-yves@hippodigital.co.uk>
2025-12-03 08:46:34 +0000

    Unified run options and smoke yaml to match others

--- performance/tests/aapApi.js
+++ performance/tests/aapApi.js
@@ -42,11 +42,13 @@ P95_THRESHOLD = 550;*/
 const BASE_URL = "https://arns-assessment-platform-api-dev.hmpps.service.justice.gov.uk";
 
 export const options = {
-  stages: [
-    { duration: "30s", target: VUS },
-    { duration: DURATION, target: VUS },
-    { duration: "30s", target: 0 },
-  ],
+  // If we have stages (from Env var), use them.
+  // If NOT, fall back to simple VUs/Duration (Better for Smoke Tests)
+  stages: __ENV.K6_STAGES ? JSON.parse(__ENV.K6_STAGES) : undefined,
+  
+  // These apply if 'stages' is undefined
+  vus: VUS,          // Starts 5 VUs immediately
+  duration: DURATION, // Runs for 30s
 
   /* Note: options will also be adjusted when running other scenarios locally: 
 Load:
