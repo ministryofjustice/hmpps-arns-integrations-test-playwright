e61dd50a126acd62cf1047bbeb83da75690797e3
JY0280 <jean-yves@hippodigital.co.uk>
2026-05-06 15:32:41 +0100

    Added assessment interface

--- utils/arnsClient.ts
+++ utils/arnsClient.ts
@@ -2,6 +2,29 @@ import { APIRequestContext, APIResponse } from '@playwright/test';
 import fs from 'fs';
 import path from 'path';
 
+export interface AssessmentStep {
+  description: string;
+  status: string;
+  actor: string;
+  statusDate: string;
+}
+export interface AssessmentGoal {
+  titleLength: number;
+  titleHash: string;
+  areaOfNeed: string;
+  relatedAreasOfNeed: string[];
+  targetDate: string | null;
+  goalStatus: string;
+  steps: AssessmentStep[];
+}
+
+export interface Assessment {
+  crn: string;
+  nomis: string | null;
+  planStatus: string;
+  goals: AssessmentGoal[];
+}
+
 export const getToken = () => {
   const tokenPath = path.join(__dirname, 'arnsAssessmentToken.json');
   return JSON.parse(fs.readFileSync(tokenPath, 'utf8')).access_token;
@@ -14,7 +37,7 @@ export const getBaseUrl = (baseUrl: string): string => {
   return 'https://arns-assessment-view-api-dev.hmpps.service.justice.gov.uk';
   };
 
-export async function viewAssessment(request: APIRequestContext, crn: string): Promise<any> {
+export async function viewAssessment(request: APIRequestContext, crn: string): Promise<Assessment[]> {
   const response = await request.get(`/sentence-plan/${crn}`);
 
   if (!response.ok()) {
