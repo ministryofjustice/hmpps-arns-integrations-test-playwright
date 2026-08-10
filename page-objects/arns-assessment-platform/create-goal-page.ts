580320aab3c022db97b613012679eb60b8b63e1e
lewis prescott <lewis.prescott@justice.gov.uk>
2026-08-10 11:18:03 +0100

    fix: area of need journey

--- page-objects/arns-assessment-platform/create-goal-page.ts
+++ page-objects/arns-assessment-platform/create-goal-page.ts
@@ -16,8 +16,8 @@ export class CreateGoalPage {
   constructor(page: Page) {
     this.page = page;
     this.searchGoal = page.locator('accessible-autocomplete-wrapper').getByRole('combobox');
-    this.relatedGoalYes = page.getByRole('group', { name: 'Is this goal related to any' }).getByLabel('Yes');
-    this.relatedGoalYNo = page.getByRole('group', { name: 'Is this goal related to any' }).getByLabel('No');
+    this.relatedGoalYes = page.getByRole('group', { name: 'Does this goal relate to any' }).getByLabel('Yes');
+    this.relatedGoalYNo = page.getByRole('group', { name: 'Does this goal relate to any' }).getByLabel('No');
     this.relatedAreaAlcohol = page.getByRole('checkbox', { name: 'Alcohol use' });
     this.startWorkingOnThisGoalYes = page.getByRole('group', { name: 'start working on this goal' }).getByLabel('Yes');
     this.whenAimToAchieveGoal = page.getByRole('radio', { name: 'In 3 months' });
