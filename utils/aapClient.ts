0db52ff652684d366026e9145f07466bae9d18ec
Kaustav Surai <Kaustav.Surai@justice.gov.uk>
2026-09-10 17:41:28 +0100

    Deleting migrated test (#215)

--- utils/aapClient.ts
+++ utils/aapClient.ts
@@ -1,6 +1,5 @@
 import { APIRequestContext, APIResponse } from '@playwright/test';
 import fs from 'fs';
-import { CreateAssessmentResult } from './aap/assessmentTypes';
 
 export const crn = Math.random().toString().substring(2, 7);
 
@@ -14,38 +13,6 @@ export const getBaseUrl = (baseUrl: string): string => {
   }
   return 'https://arns-assessment-platform-api-dev.hmpps.service.justice.gov.uk';
 };
-
-export async function createAssessment(request: APIRequestContext): Promise<CreateAssessmentResult> {
-  const response: APIResponse = await request.post('/command', {
-    data: {
-      commands: [
-        {
-          type: 'CreateAssessmentCommand',
-          assessmentType: 'TEST',
-          formVersion: '1.0',
-          properties: {},
-          user: { id: 'test-user', name: 'Test User' },
-          identifiers: {
-            CRN: crn,
-          },
-          flags: ['SAN_BETA'],
-        },
-      ],
-    },
-  });
-
-  if (!response.ok()) {
-    throw new Error(`CreateAssessment failed: ${response.status()} ${response.statusText()}`);
-  }
-
-  const body: CreateAssessmentResult = await response.json();
-
-  // Check the UUID exists
-  const assessmentUuid = body?.commands?.[0]?.result?.assessmentUuid;
-  if (!assessmentUuid) throw new Error('No assessmentUuid found');
-
-  return body; // Return full API response
-}
 
 export async function queryAssessment(request: APIRequestContext, assessmentUuid: string) {
   const response = await request.post('/query', {
