6eb6f6e9d6fac81f93db4940307944a915090c38
lewis prescott <lewis.prescott@justice.gov.uk>
2026-09-16 09:38:11 +0100

    test: drug use analysis questions

--- page-objects/strengths-and-needs/drug-page.ts
+++ page-objects/strengths-and-needs/drug-page.ts
@@ -44,6 +44,7 @@ export default class DrugPage extends AapPage {
     await this.saveAndContinue.click();
 
     await this.practitionerAnalysis.click();
+    await this.unknown.click();
     await this.practionerAnalysisQuestions();
     await this.markAsComplete.click();
     await this.sectionComplete('Drug use');
